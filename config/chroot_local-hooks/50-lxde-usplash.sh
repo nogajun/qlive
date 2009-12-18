97af383c0684efcaa31c9d61fd07d644bd59fc3b
Jun NOGATA <nogajun@gmail.com>
2009-12-19 04:39:25 +0900

    delete unused files and fix gitignore

--- config/chroot_local-hooks/50-lxde-usplash.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-# set usplash screen theme CrunchyBranch
-
-[ -f /usr/lib/usplash/CrunchyBranch.so ] || exit 0
-
-/usr/sbin/update-alternatives --set usplash-artwork.so /usr/lib/usplash/CrunchyBranch.so
-
